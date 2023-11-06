be1189fac0ffe012d71d1f7f333fe03095f3e22f
sergioSHKLR <sergio@shklr.org>
2023-11-06 02:29:05 -0600

    init

---
+++ framework/js/script.js
@@ -0,0 +1,179 @@
+
+
+// DARK MODE --------------------------------------------------
+
+function activeDark2() {
+    var element = document.body;
+    element.classList.add("dark-mode");
+    var element = document.getElementById("col2-dm-btn");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col2-lm-btn");
+    element.classList.toggle("show");
+}
+
+function inactiveDark2() {
+    var element = document.body;
+    element.classList.remove("dark-mode");
+    var element = document.getElementById("col2-lm-btn");
+    element.classList.toggle("show");
+    var element = document.getElementById("col2-dm-btn");
+    element.classList.toggle("hide");
+}
+
+function activeDark3() {
+    var element = document.body;
+    element.classList.add("dark-mode");
+    var element = document.getElementById("col3-dm-btn");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col3-lm-btn");
+    element.classList.toggle("show");
+}
+
+function inactiveDark3() {
+    var element = document.body;
+    element.classList.remove("dark-mode");
+    var element = document.getElementById("col3-lm-btn");
+    element.classList.toggle("show");
+    var element = document.getElementById("col3-dm-btn");
+    element.classList.toggle("hide");
+}
+
+//---------------------------------------------------------------
+
+// FULLSCREEN ---------------------------------------------------
+
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen2() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    var element = document.getElementById("col2-fs-btn");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col2-ns-btn");
+    element.classList.toggle("show");
+}
+
+/* Close fullscreen */
+function closeFullscreen2() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    var element = document.getElementById("col2-ns-btn");
+    element.classList.toggle("show");
+    var element = document.getElementById("col2-fs-btn");
+    element.classList.toggle("hide");
+}
+
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen3() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    var element = document.getElementById("col3-fs-btn");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col3-ns-btn");
+    element.classList.toggle("show");
+}
+
+/* Close fullscreen */
+function closeFullscreen3() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    var element = document.getElementById("col3-ns-btn");
+    element.classList.toggle("show");
+    var element = document.getElementById("col3-fs-btn");
+    element.classList.toggle("hide");
+
+}
+
+//--------------------------------------------------------------
+
+// SHADE ------------------------------------------------------
+
+function shadesOn() {
+    var element = document.getElementById("ext-iframe");
+    element.classList.add("shades-on");
+    element.classList.remove("shades-off");
+    var element = document.getElementById("col3-sh-on-btn");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col3-sh-off-btn");
+    element.classList.toggle("show");
+}
+
+function shadesOff() {
+    var element = document.getElementById("ext-iframe");
+    element.classList.add("shades-off");
+    element.classList.remove("shades-on");
+    var element = document.getElementById("col3-sh-off-btn");
+    element.classList.toggle("show");
+    var element = document.getElementById("col3-sh-on-btn");
+    element.classList.toggle("hide");
+}
+
+//--------------------------------------------------------------
+
+/*
+function toggleMenu() {
+    var x = document.getElementById("col1-body");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  */
+
+function menuOn() {
+    document.getElementById("col1-body").style.display = 'inline-block';
+    document.getElementById("col2-menu-on").style.display = 'none';
+    document.getElementById("col2-menu-off").style.display = 'inline-block';
+}
+
+function menuOff() {
+    document.getElementById("col1-body").style.display = 'none';
+    document.getElementById("col2-menu-off").style.display = 'none';
+    document.getElementById("col2-menu-on").style.display = 'inline-block';
+}
+
+// SCROLL INDICATOR
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+        scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
